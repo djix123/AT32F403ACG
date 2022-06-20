423efa2a1e3e6e011f9b0213a29c832127cb80fb
Jonathan Giles <jonathangiles@fastmail.fm>
2022-06-20 11:17:04 -0400

    Update comments ...

--- Inc/delay.h
+++ Inc/delay.h
@@ -7,11 +7,6 @@ extern "C" {
 
 #include "at32f403a_407.h"
 
-//void delay_init(void);
-//void delay_us(uint32_t nus);
-//void delay_ms(uint16_t nms);
-//void delay_sec(uint16_t sec);
-
 void systick_config(void);
 void delay_decrement(void);
 void delay_ms(uint32_t count);
